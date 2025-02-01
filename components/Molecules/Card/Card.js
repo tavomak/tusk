a0e00bcf4a842ccbca3314074f9a1c9a5dbe9b4d
Gustavo Rivero <tavomak@gmail.com>
2025-01-31 21:28:59 -0300

    fix(design): Team card

---
+++ components/Molecules/Card/Card.js
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+import { FaLinkedin } from 'react-icons/fa';
+
+const Card = ({ item, handleClick }) => (
+  <a
+    key={item.id}
+    className="relative block border cursor-pointer group border-neutral-800 rounded-xl"
+    onClick={(e) => handleClick(e, item)}
+    href="!#"
+  >
+    <div className="inset-0 block transition-all duration-300 opacity-0 md:absolute bg-emerald-950 overlay group-hover:opacity-50 rounded-xl" />
+    <Image
+      src={item?.image?.url}
+      alt={item?.name}
+      width={390}
+      height={390}
+      className="w-full shadow rounded-xl"
+      priority
+    />
+    <div className="inset-0 p-4 text-white md:justify-end group-hover:flex-col md:hidden md:absolute group-hover:flex">
+      <div className="mb-2">
+        <span>{item?.linkedin && <FaLinkedin className="text-2xl" />}</span>
+      </div>
+      <h3 className="text-xl font-bold">{item.name}</h3>
+      <p className="text-sm font-bold">{item.position}</p>
+      <p className="text-sm">{item.email}</p>
+      {item?.officeTag && (
+        <div className="mt-2">
+          <span className="px-4 rounded-full bg-primary-color">
+            {item?.officeTag}
+          </span>
+        </div>
+      )}
+    </div>
+  </a>
+);
+
+export default Card;
