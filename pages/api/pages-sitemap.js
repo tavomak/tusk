1d9150eeb7bc875129a5d33535737bb04cb531cd
Gustavo Rivero <tavomak@gmail.com>
2025-02-06 10:35:25 -0300

    fix(Seo): External URL and en paths

--- pages/api/pages-sitemap.js
+++ pages/api/pages-sitemap.js
@@ -3,9 +3,17 @@ import { SitemapStream, streamToPromise } from 'sitemap';
 import { Readable } from 'stream';
 
 const sitemapBlog = async (req, res) => {
-  const staticPages = navItems
+  const staticEsPages = navItems
     .filter((item) => !item.external)
     .map((item) => ({ url: item.path }));
+
+  const staticEnPages = navItems
+    .filter((item) => !item.external)
+    .map((item) => ({
+      url: `en${item.path}`,
+    }));
+
+  const staticPages = [...staticEnPages, ...staticEsPages];
 
   const stream = new SitemapStream({ hostname: `https://${req.headers.host}` });
 
